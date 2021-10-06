142cc5a5ef7d793a4f931df7ea31e73a63a504b9
dipsonu10 <dipsonu10@hotmail.com>
2021-10-06 11:00:29 +0530

    Completed the a2.cpp, a3.cpp, a4.cpp

---
+++ Lab8/a5.cpp
@@ -0,0 +1,15 @@
+/*an election is contested by 3 candidates and the candidates are numbered from 1 to 3 
+the voting is done by making the candidate number on the ballot paper WAP using constructor and destructor to 
+i) cast the vote for user supplied n votes
+ii) read the ballots and count the votes for each candidates*/
+#include <iostream>
+using std::cin;
+using std::endl;
+using std::cout;
+
+
+int main(int argc, char** argv) {
+    
+    remove(argv[0]);
+    return EXIT_SUCCESS;
+}
