07bbc01ca0ed2470278feb5d94b36772327eb0f6
DipankarDas <dipsonu10@hotmail.com>
2021-12-08 11:24:37 +0530

    Added the finalLabExam

--- FinalLabExam/a1.cpp
+++ FinalLabExam/a1.cpp
@@ -2,9 +2,30 @@
 #include <iostream>
 using namespace std;
 
+class Number{
+    int a, b;
+    public:
+    Number(int x, int y):a(x),b(y){}
+
+    void display(){
+        cout << "a: " << a << " b: " << b << endl;
+    }
+
+    friend void addition(Number &);
+};
+
+void addition(Number& ob){
+    int k = ob.a + ob.b;
+    cout << "Sum: " << k << endl;
+}
 
 int main(int argc, char** argv) {
-    
+    cout << "Enter the value of a, b: ";
+    int a, b;
+    cin >> a >> b;
+    Number num(a, b);
+    num.display();
+    addition(num);
     remove(argv[0]);
     return EXIT_SUCCESS;
 }
--- FinalLabExam/a2.cpp
+++ FinalLabExam/a2.cpp
@@ -2,9 +2,58 @@
 #include <iostream>
 using namespace std;
 
+template <class T>
+class Array{
+    T *arr;
+    int size;
+
+    public:
+    Array(int n){
+        size = n;
+        arr = new T[size];
+    }
+    ~Array(){
+        delete[] arr;
+    }
+
+    void input(){
+        cout << "Enter the data" << endl;
+        for (int i = 0; i < size;i++)
+            cin >> arr[i];
+    }
+    void display(){
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << endl;
+    }
+
+    void sortit(){
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size-i-1; j++)
+            {
+                if(arr[j] > arr[j+1]){
+                    T temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+};
 
 int main(int argc, char** argv) {
-    
+    Array<int> ob1(5);
+    ob1.input();
+    ob1.sortit();
+    ob1.display();
+
+    Array<float> ob2(5);
+    ob2.input();
+    ob2.sortit();
+    ob2.display();
     remove(argv[0]);
     return EXIT_SUCCESS;
 }
